9869a8f4f08aa94ae511327dcc3ca775343cf7f9
Raine Revere <raine@cybersemics.org>
2020-12-18 10:39:44 -0700

    Add support for --type option

--- cli.js
+++ cli.js
@@ -7,24 +7,21 @@ program
   .arguments("<isbn>")
   .option("-t, --type <api>", "api to use, defaults to all")
   .action((isbn, options) => {
-    if (!options.type) {
-      bookcovers
-        .withIsbn(isbn)
-        .then(results => {
-          for (let [source, images] of Object.entries(results)) {
-            if (images === null) continue;
-            for (let [qualifier, url] of Object.entries(images)) {
-              if (qualifier === "error") continue;
-              console.log(source, qualifier, url);
-            }
+    bookcovers
+      .withIsbn(isbn, program.opts())
+      .then(results => {
+        for (let [source, images] of Object.entries(results)) {
+          if (images === null) continue;
+          for (let [qualifier, url] of Object.entries(images)) {
+            if (qualifier === "error") continue;
+            console.log(source, qualifier, url);
           }
-        })
-        .catch(e => {
-          console.error(e);
-          process.exit(1);
-        });
-      return;
-    }
+        }
+      })
+      .catch(e => {
+        console.error(e);
+        process.exit(1);
+      });
   });
 
 program.parse(process.argv);
