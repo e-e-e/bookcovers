9ae566a5a47dff8600077b03b0b757c7e45ab04e
Benjamin Forster <12589522+e-e-e@users.noreply.github.com>
2021-02-21 21:54:08 +0200

    Add prettier (#22)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { Isbn } = require("library-lib");
 const services = {
   amazon: require("./services/amazon_books/amazon_books"),
   google: require("./services/google_books/google_books"),
-  openLibrary: require("./services/open_library/open_library")
+  openLibrary: require("./services/open_library/open_library"),
 };
 
 function printFailure(err) {
@@ -14,22 +14,28 @@ async function fetchImages(isbn, options = {}) {
   const isbnVariants = isbnData.isIsbn10
     ? [isbnData.toIsbn10(), isbnData.toIsbn13()]
     : [isbnData.toIsbn13(), isbnData.toIsbn10()];
-  const getWithService = service => service
-    .get(isbnVariants[0])
-    .then(res => res || service.get(isbnVariants[1]))
-    .catch(printFailure);
+  const getWithService = (service) =>
+    service
+      .get(isbnVariants[0])
+      .then((res) => res || service.get(isbnVariants[1]))
+      .catch(printFailure);
   if (options.type) {
     return {
-      [options.type]: await getWithService(services[options.type])
+      [options.type]: await getWithService(services[options.type]),
     };
   }
-  const results = await Promise.all(Object.values(services).map(getWithService));
-  return Object.keys(services).reduce((accum, key, i) => ({
-    ...accum,
-    [key]: results[i]
-  }), {});
+  const results = await Promise.all(
+    Object.values(services).map(getWithService)
+  );
+  return Object.keys(services).reduce(
+    (accum, key, i) => ({
+      ...accum,
+      [key]: results[i],
+    }),
+    {}
+  );
 }
 
 module.exports = {
-  withIsbn: fetchImages
+  withIsbn: fetchImages,
 };
